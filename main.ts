785a3929d0ea23da7d09878bb08461ffd2a3eb26
Paul Boon <gvr37.p.l.boon@gmail.com>
2018-07-01 23:12:23 +0200

    p

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 /// <reference path="node_modules/utilsx/utils.ts" />
-/// <reference path="node_modules/vectorx/vector.ts" />
+/// <reference path="node_modules/rect2x/rect.ts" />
+
 
 var jumbotronElement = document.querySelector('.jumbotron') as HTMLElement
 
@@ -13,8 +14,34 @@ canvas.style.position = "absolute"
 jumbotronElement.appendChild(canvas)
 
 
-loop((dt) => {
-    ctxt.clearRect(0,0,500,500)
+var width = new Vector2(canvas.width,canvas.height) 
 
-    ctxt.fillRect(400,0,400,1000)
-})
+
+// loop((dt) => {
+    ctxt.clearRect(0,0,width.x,width.y)
+    var tilesize = new Vector2(50,50)
+    
+    var grid = new Vector2(Math.ceil(width.x / tilesize.x),Math.ceil(width.y / tilesize.y))
+    grid.loop((v) => {
+        var pos = v.c().mul(tilesize)
+        var rect = new Rect(pos,tilesize)
+        slantedLine(rect, Math.random() > 0.5)
+    })
+
+    
+// })
+
+function slantedLine(rect:Rect,right:boolean){
+    if(right){
+        line(ctxt,rect.getPoint(new Vector2(-1,-1)),rect.getPoint(new Vector2(1,1)))
+    }else{
+        line(ctxt,rect.getPoint(new Vector2(1,-1)),rect.getPoint(new Vector2(-1,1)))
+    }
+}
+
+function line(ctxt:CanvasRenderingContext2D,a:Vector2,b:Vector2){
+    ctxt.beginPath()
+    ctxt.moveTo(a.x,a.y)
+    ctxt.lineTo(b.x,b.y)
+    ctxt.stroke()
+}
---
+++ rotatingLine.ts
@@ -0,0 +1,22 @@
+/// <reference path="node_modules/rect2x/rect.ts" />
+
+
+class RotatingLine{
+    a:Vector2
+    b:Vector2
+    
+
+    constructor(a:Vector2,b:Vector2){
+        this.a = a
+        this.b = b
+    }
+
+    draw(){
+        line
+    }
+
+    update(){
+
+    }
+
+}
